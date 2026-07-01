83a3f6a95d50ad1d5530c81b0b5bdd538f56ee0f
yashamking12345-dotcom <yashamking12345@gmail.com>
2026-07-02 02:06:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
+    templates.env.cache.clear()  # 🧹 پاک کردن کش
     return templates.TemplateResponse(
         "index.html",
         {"request": request}
